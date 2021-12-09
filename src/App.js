498262852183bf0c58fc200f18b1acee7707acae
ElvisVega <elvis.vega24724@gmail.com>
2021-12-09 11:37:35 -0400

    Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faPencilAlt} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <pagina>
+       <head>
+      
+      <script src="https://kit.fontawesome.com/0a6846680d.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+<footer class="foot">
+<div className='main-footer'>
+<h1 class="Titulo"> CONTACT US</h1>
+<div className='home'>
+<FontAwesomeIcon className='homeI' icon={faHome} />
+<h2>Cochabamba</h2>
+</div>
+
+<div className="phone">
+<FontAwesomeIcon className='PhoneI'icon={faPhone} />   
+<h2>722224537</h2>
+</div>
+
+
+<div className="email">
+<FontAwesomeIcon className='EmailI' icon={faPencilAlt} />     
+<h2>aplicacion@empleosya.com.bo</h2>
+</div>
+</div>
+</footer>
+
+    </body>
+
+
+      
+    </pagina>
   );
 }
 
